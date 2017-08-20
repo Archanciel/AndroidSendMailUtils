3fc1db32386958aa5923287078881331f9d25572
Archanciel <jp.schnyder@gmail.com>
2017-08-20 18:04:12 +0200

    partial html order mail

--- orderMedic.py
+++ orderMedic.py
@@ -1,34 +1,29 @@
-#-*-coding:utf8;-*-
-#qpy:3
-#qpy:console
-
 import sendmail.sendmail as sm
 import android
 import re
+from io import StringIO
 
 TITLE='Commande de médicaments'
-#EMAIL_ADDRESS='katchenjunga@gmail.com'
 EMAIL_ADDRESS='baccaph@planet.ch'
+#EMAIL_ADDRESS='katchenjunga@gmail.com'
 EMAIL_TITLE="Commande de médicaments pour Jean-Pierre Schnyder"
 EMAIL_BCC='jp.schnyder@gmail.com'
 
+def toHtmlTable(buf, lol):
+  buf.write('<table border="0">\n')
+  for sublist in lol:
+    buf.write('  <tr>\n')
+    buf.write('  <td>' + '</td><td>'.join(sublist) + '<td>\n')
+    buf.write('  </tr>\n')
+  buf.write('</table>')
+  
 droid=android.Android()
-commande="""
+commandeTxt="""
 Bonjour,
 
-Je souhaite vous commander les médicaments  suivants:
+Je souhaite vous commander les médicaments suivants. Avec mes remerciwnment et mes salutations. JP Schnyder.
 
-
-Betaferon                                     1 boîte
-Sirdalud Retard 6 mg                 {0} boîte(s)
-Lioresal 25 mg                            {1} boîte(s)
-{2}{3}{4}
-
-Avec mes remerciements et mes cordiales salutations,
-Jean-Pierre Schnyder\n"""
-valverdeLine ="Valverde Forte                             {0} boîte(s)\n"
-zincGlukoLine="Zinc Glukonat 30mg                   {0} boîte(s)\n"
-magnesiumLine="Magnesium Compl 100 caps    {0} boîte(s)\n"
+"""
 
 pattern = r"(\d?)\W*(\d)\W*(\d)\W*(\d)\W*(\d)"
 
@@ -48,24 +43,25 @@ valvQt=parms[2]
 zincQt=parms[3]
 magnQt=parms[4]
 
+commande = [['Betaferon', '1', 'bo&icirc;te'],
+            ['Sirdalud Retard 6 mg', str(sirdaQt), 'bo&icirc;te(s)'],
+            ['Lioresal 25 mg', str(lioQt), 'bo&icirc;te(s)']]
+
 if int(valvQt) > 0:
-    valverdeLine = valverdeLine.format(valvQt)
-else:
-    valverdeLine = ""
-    
+    commande.append(['Valverde Forte', str(valvQt), 'bo&icirc;te(s)'])
+
 if int(zincQt) > 0:
-    zincGlukoLine = zincGlukoLine.format(zincQt)
-else:
-    zincGlukoLine = ""
+    commande.append(['Zinc Glukonat 30mg', str(zincQt), 'bo&icirc;te(s)'])
 
 if int(magnQt) > 0:
-    magnesiumLine = magnesiumLine.format(magnQt)
-else:
-    magnesiumLine = ""
+    commande.append(['Magnesium Compl 100 caps', str(magnQt), 'bo&icirc;te(s)'])
 
-commande=commande.format(sirdaQt,lioQt,valverdeLine,zincGlukoLine,magnesiumLine)
-    	
-sm.sendMailWithBodyTo(commande, EMAIL_ADDRESS, EMAIL_BCC, EMAIL_TITLE,'JP Schnyder')
+buf = StringIO()
+
+toHtmlTable(buf, commande)
+commandeHTML = buf.getvalue()
+
+sm.sendMailWithTxtAndHtmlBodyTo(commandeTxt, commandeHTML, EMAIL_ADDRESS, EMAIL_BCC, EMAIL_TITLE,'JP Schnyder')
 msg="Commande de médicaments ({0} Lio, {1} Sirda, {2} Valv, {3} Zinc, {4} Magn) envoyée à {5}".format(lioQt,sirdaQt,valvQt,zincQt,magnQt,EMAIL_ADDRESS)
     
 droid.dialogCreateAlert('',msg)

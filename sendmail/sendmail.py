8d7ff976968c1a682f8d678b7cb4dc9812e886e9
Archanciel <jp.schnyder@gmail.com>
2017-08-20 18:06:02 +0200

    add send only html mail

--- sendmail/sendmail.py
+++ sendmail/sendmail.py
@@ -85,6 +85,47 @@ def sendemailTextAndHtml(email_name, email_user, email_pswd, mailto, mailbcc, su
     print(err)
     return 0
 
+def sendemailHtml(email_name, email_user, email_pswd, mailto, mailbcc, subject, bodyHtml):
+
+  # DON'T CHANGE THIS!
+  # ...unless you're rewriting this script for your own SMTP server!
+  smtp_server = 'smtp.mail.yahoo.com'
+  smtp_port = 465
+
+  # Build an SMTP compatible message
+  # Note special utf-8 settings for subject and body !
+#  msg = MIMEMultipart('alternative') causes the connection to close !!b
+  msg = MIMEMultipart()
+  msg['Subject'] = Header(subject, 'utf-8')
+  msg['To'] = mailto
+  msg['From'] = email_name + " <" + email_user + ">"
+  msg.attach(MIMEText(bodyHtml, 'html'))
+
+  toaddrs = [mailto] + [mailbcc]
+  
+  # Attempt to connect and send the email
+  try:
+    smtpObj = '' # Declare within this block.
+    # Check for SMTP over SSL by port number and connect accordingly
+    if( smtp_port == 465):
+      smtpObj = smtplib.SMTP_SSL(smtp_server,smtp_port)
+    else:
+      smtpObj = smtplib.SMTP(smtp_server,smtp_port)
+    smtpObj.ehlo()
+    # StartTLS if using the default TLS port number
+    if(smtp_port == 587):
+      #smtpObj.starttls()
+      smtpObj.ehlo
+    # Login, send and close the connection.
+    smtpObj.login(email_user, email_pswd)
+    smtpObj.sendmail(email_user, toaddrs, msg.as_string())
+    smtpObj.close()
+    return 1  # Return 1 to denote success!
+  except Exception as err:
+    # Print error and return 0 on failure.
+    print(err)
+    return 0
+
 import sys
 import android
 import pickle
@@ -119,6 +160,14 @@ def sendMailWithBodyTo(bodyText,targetEmail,mailbcc,mailSubject,mailName):
 
 def sendMailWithTxtAndHtmlBodyTo(bodyText,bodyHtml,targetEmail,mailbcc,mailSubject,mailName):
     if (sendemailTextAndHtml(mailName, cred['usr'], cred['pw'], targetEmail, mailbcc, mailSubject, bodyText, bodyHtml)):
+        pass
+    else:
+        # Exit with error if email is not sent successfully
+        droid.makeToast('email failed')
+        sys.exit(1)   
+        
+def sendMailWithHtmlBodyTo(bodyHtml,targetEmail,mailbcc,mailSubject,mailName):
+    if (sendemailHtml(mailName, cred['usr'], cred['pw'], targetEmail, mailbcc, mailSubject, bodyHtml)):
         pass
     else:
         # Exit with error if email is not sent successfully
